112f64d6266aba6fbe1aa3989779857d0f9fb73a
Raffaele Apetino <ape.97@hotmail.it>
2022-03-17 21:50:26 +0100

    mock view controller for apple watch

---
+++ TimatoWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  TimatoWatch WatchKit Extension
+//
+//  Created by Raffaele Apetino on 17/03/22.
+//  Copyright © 2022 Raffaele Apetino. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ TimatoWatch WatchKit Extension/NotificationController.swift
@@ -0,0 +1,35 @@
+//
+//  NotificationController.swift
+//  TimatoWatch WatchKit Extension
+//
+//  Created by Raffaele Apetino on 17/03/22.
+//  Copyright © 2022 Raffaele Apetino. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
